88fcb4a57ace90b26ca91cad1f22ae70c58b35a4
Thanawat Thipkaew <6710110177@psu.ac.th>
2025-03-06 23:46:30 +0700

    test:PrimeListTest

---
+++ tests/test_number_utils.py
@@ -0,0 +1,37 @@
+from coe_6710110177.number_utils import is_prime_list
+import unittest
+
+class PrimeListTest(unittest.TestCase):
+
+    def test_give_1_2_3_is_prime(self):
+        prime_list = [1, 2, 3]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+        
+    def test_empty_list(self):
+        prime_list = []
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime) 
+
+    def test_single_prime(self):
+        prime_list = [2]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_single_non_prime(self):
+        prime_list = [4]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_mixed_prime_and_non_prime(self):
+        prime_list = [2, 4, 7, 8, 13]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_large_prime_list(self):
+        prime_list = [101, 103, 107, 109, 113]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+if __name__ == '__main__':
+    unittest.main()
