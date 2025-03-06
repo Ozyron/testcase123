9ac341f16591f323d8c328c8ed6b844521386b30
Thanawat Thipkaew <6710110177@psu.ac.th>
2025-03-07 00:03:08 +0700

    test:TestFizzBuzz

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,15 @@
+import unittest
+from coe_6710110177.fizzbuzz import fizzbuzz
+class TestFizzBuzz(unittest.TestCase):
+    
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+    
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+    
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+
+if __name__ == '__main__':
+    unittest.main()
